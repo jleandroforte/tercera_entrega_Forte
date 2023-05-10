240494cab2dfcc35013347c6032f64f6e653c25d
Leandro Forte <jleandroforte@hotmail.com>
2023-05-10 18:56:56 -0300

    v8

--- AppBlog/views.py
+++ AppBlog/views.py
@@ -58,29 +58,24 @@ def crear_etiquetas(request):   # funcion para crear etiquetas.
 # funcion para buscar etiquetas 
 
 def buscar_etiquetas(request):
-    # Obtener el valor del parámetro 'etiquetas_del_post' desde la URL
     nombre_de_busqueda = request.GET.get('etiquetas_del_post')
     print(nombre_de_busqueda)
 
     if nombre_de_busqueda:
-        # Si se proporciona un valor de búsqueda
-        # Filtrar las etiquetas que contengan el valor de búsqueda
+        
         lista = Etiquetas.objects.filter(etiquetas_del_post__icontains=nombre_de_busqueda)
         
         if len(lista) != 0:
-            # Si se encontraron etiquetas que coinciden con la búsqueda
-            # Crear una instancia del formulario para mostrarlo en la plantilla
+           
             form = Formulario_Etiquetas()
             contexto = {
-                'etiquetas_del_post': lista,  # Pasar las etiquetas encontradas al contexto
-                'form': form  # Pasar el formulario al contexto
+                'etiquetas_del_post': lista,  
+                'form': form  
             }
             return render(request, 'AppBlog/buscar_etiquetas.html', context=contexto)
         else:
-            # Si no se encontraron etiquetas que coincidan con la búsqueda
             mensaje = "No se encontraron etiquetas con ese nombre."
             return render(request, 'AppBlog/buscar_etiquetas.html', {"mensaje": mensaje, "form": Formulario_Etiquetas()})
     else:
-        # Si no se proporcionó ningún dato de búsqueda
-        # Mostrar solo el formulario vacío en la plantilla
+        
         return render(request, 'AppBlog/buscar_etiquetas.html', {"form": Formulario_Etiquetas()})
